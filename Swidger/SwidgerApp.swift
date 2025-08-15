3bcc0219d66a57ebde3f083eadcede5cf0ca0382
Jochen Bernard <jochenbernard@gmail.com>
2025-08-15 09:28:24 +0200

    Initial Commit

---
+++ Swidger/SwidgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwidgerApp.swift
+//  Swidger
+//
+//  Created by Jochen Bernard on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwidgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
